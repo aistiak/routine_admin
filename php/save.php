533720e55bd09d56a343dc695cb555c9180e2506
Your Name <you@example.com>
2018-05-21 05:14:31 +0600

    almost done

---
+++ php/save.php
@@ -0,0 +1,19 @@
+<?php
+
+   if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+	    $js_string = $_POST['js_string'];
+		
+		$file = "data.json";
+		file_put_contents($file,$js_string);
+		
+		
+		
+	  
+		
+		
+
+		
+	    
+   }
+
+?>
