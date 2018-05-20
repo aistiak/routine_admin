41e06a1830ca48dcf597faae5329ba8b9fcad0a8
Your Name <you@example.com>
2018-05-20 15:50:31 +0600

    added php scripts

---
+++ php/handel.php
@@ -0,0 +1,19 @@
+<?php
+
+   if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+	    $js_string = $_GET['js_string'];
+		
+		echo "hello"."  ".$js_string  ;
+		
+		$string = file_get_contents("data.json");
+        $json= json_decode($string, true);
+		
+		
+		
+		
+
+		
+	    
+   }
+
+?>
