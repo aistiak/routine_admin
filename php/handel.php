84226a815fd1ba18f2fa57d6dee96d5f13d6ffb2
Your Name <you@example.com>
2018-05-21 01:24:56 +0600

    object loaded yaeee =D

--- php/handel.php
+++ php/handel.php
@@ -1,14 +1,16 @@
 <?php
 
    if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-	    $js_string = $_GET['js_string'];
+	    //$js_string = $_GET['js_string'];
 		
-		echo "hello"."  ".$js_string  ;
+		//echo "hello"."  ".$js_string  ;
 		
 		$string = file_get_contents("data.json");
-        $json= json_decode($string, true);
+        $json= json_encode($string);
 		
 		
+		
+	echo $string ;	  
 		
 		
 
